a8ae47dd77f13be1ce470ec16e15f88aa1004cc4
macafsn16 <103944518+macafsn16@users.noreply.github.com>
2023-06-11 14:53:41 -0300

    Actualizacion traduccion

--- src/components/RandomCards.js
+++ src/components/RandomCards.js
@@ -1,15 +1,21 @@
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
 
 function RandomCards() {
+  /* ===== traductor ===== */
+  const [ t, i18n ] = useTranslation ("traduccion");
+
   return (
     <CardGroup className="m-4 p-4 text-dark" >
       <Card className="m-4 p-4 " border="light">
         <Card.Img variant="top" src="https://www.pierandrei.com.ar/wp-content/uploads/2021/02/PAGO-EFECTIVO.png" />
         <Card.Body>
-          <Card.Title>Miércoles de 2x1!</Card.Title>
+          <Card.Title>{t("randomcards.miercoles")}</Card.Title>
           <Card.Text>
-            Todos los miércoles 2x1 en postres...
+            {t("randomcards.promo")}
           </Card.Text>
         </Card.Body>
       </Card>
@@ -18,16 +24,16 @@ function RandomCards() {
         <Card.Body className="mt-4">
           <Card.Title>15% OFF!!</Card.Title>
           <Card.Text>
-            Todos los jueves y viernes 15% OFF en envíos de 20 a 23hs!!
+          {t("randomcards.promo2")}
           </Card.Text>
         </Card.Body>
       </Card>
       <Card className="m-4 p-4" border="light">
         <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIXMTOjt5i0Z7r_vK3CatU8p_Iu2cdqsAOhOJnpjwPFE7AKBKHLfZgW8tKh1quAN1ct4c&usqp=CAU" />
         <Card.Body>
-          <Card.Title>Domingo Turbo</Card.Title>
+          <Card.Title>{t("randomcards.domingo")}</Card.Title>
           <Card.Text>
-            Los domingos podes hacer tu pedido con la opción turbo y que llegue mas rápido!
+          {t("randomcards.promo3")}
           </Card.Text>
         </Card.Body>
       </Card>
